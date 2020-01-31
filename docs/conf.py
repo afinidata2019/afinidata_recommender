7d695aaf31668c772d74cacaa438eecd8ee5f157
paloderosa <onewaypincheguey@gmail.com>
2020-01-30 23:26:22 -0600

    corrected conf.py to see if it builds

--- docs/conf.py
+++ docs/conf.py
@@ -60,7 +60,7 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
+master_doc = 'index'
 
 # -- Extension configuration -------------------------------------------------
 
@@ -68,3 +68,5 @@ html_static_path = ['_static']
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
+
+
